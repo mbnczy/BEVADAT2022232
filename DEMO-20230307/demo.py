1a0c420e432971081b6c65967e0b193b320ec891
mbnczy <banoczy.martin@gmail.com>
2023-03-07 10:30:51 +0100

    gyak02

---
+++ DEMO-20230307/demo.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def print_hello_world():
+    print("Hello World!")
+
+def return_hello_world():
+    return "Hello World!"
+
+def create_1d_np_array(size: int = 1) -> np.array:
+    return np.zeros(size)

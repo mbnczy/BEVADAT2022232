a9736d5e0bbfa8bfec6b97e8eeac05f1399890b0
Banoczy Martin <banoczy.martin@gmail.com>
2023-04-04 00:08:52 +0200

    gyak05 modified

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -10,43 +10,46 @@ class KNNClassifier:
         self.k = k
         self.test_split_ratio = test_split_ratio
 
-    @property
-    def k_neighbors(self):
+    def get_k(self):
         return self.k
+
+    k_neighbors = property(fget=get_k)
 
     @staticmethod
     def load_csv(csv_path: str) -> Tuple[np.ndarray, np.ndarray]:
         np.random.seed(42)
         dataset = np.genfromtxt(csv_path, delimiter=",")
-        np.random.shuffle(dataset)
-        x, y = dataset[:, :-1], dataset[:, -1]
+        np.random.shuffle(
+            dataset,
+        )
+        x, y = dataset[:, :4], dataset[:, -1]
         return x, y
 
-    def train_test_split(self, features: np.ndarray, labels: np.ndarray):
+    def train_test_split(self, features: np.ndarray, labels: np.ndarray) -> None:
         test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
 
-        assert len(features) == test_size + train_size, "size mismatch!"
+        assert len(features) == test_size + train_size, "Size mismatch!"
 
-        self.x_train, self.y_train = features[:train_size, :], labels[:train_size]
-        self.x_test, self.y_test = (
-            features[train_size : train_size + test_size, :],
-            labels[train_size : train_size + test_size],
-        )
+        x_train, y_train = features[:train_size], labels[:train_size]
+        x_test, y_test = features[train_size:], labels[train_size:]
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
 
-    def eucledian(self, element_of_x: np.ndarray) -> np.ndarray:
+    def euclidean(self, element_of_x: np.ndarray) -> np.ndarray:
         return np.sqrt(np.sum((self.x_train - element_of_x) ** 2, axis=1))
 
-    def predict(self, x_test: np.ndarray):
+    def predict(self, x_test: np.ndarray) -> np.ndarray:
         labels_pred = []
         for x_test_element in x_test:
-            distances = self.euclidean(x_test_element)
+            distances = self.euclidean(self, x_test_element)
             distances = np.array(sorted(zip(distances, self.y_train)))
-
             label_pred = mode(distances[: self.k, 1], keepdims=False).mode
             labels_pred.append(label_pred)
 
-        self.y_preds = np.array(labels_pred, dtype=np.int64)
+        self.y_preds = np.array(labels_pred, dtype=np.int32)
 
     def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()

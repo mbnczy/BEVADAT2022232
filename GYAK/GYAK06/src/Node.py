0253cb325fd91b83a8c67c5b614b20e8c9a926ec
Banoczy Martin <banoczy.martin@gmail.com>
2023-04-04 16:39:37 +0200

    dtc and node init

--- GYAK/GYAK06/src/Node.py
+++ GYAK/GYAK06/src/Node.py
@@ -1,19 +1,13 @@
-class Node:
-    def __init__(
-        self,
-        feature_index=None,
-        threshold=None,
-        left=None,
-        right=None,
-        value=None,
-        info_gain=None,
-    ) -> None:
-        # leaf
-        self.value = value
-
-        # dis
+class Node():
+    def __init__(self, feature_index=None, threshold=None, left=None, right=None, info_gain=None, value=None):
+        ''' constructor ''' 
+        
+        # for decision node
         self.feature_index = feature_index
         self.threshold = threshold
-        self.right = right
         self.left = left
-        self.info_gain = info_gain
+        self.right = right
+        self.info_gain = info_gain 
+        
+        # for leaf node
+        self.value = value

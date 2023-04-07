65d286f7994d458fc6f096ad114b1afbe16a9589
Banoczy Martin <banoczy.martin@gmail.com>
2023-04-07 23:12:33 +0200

    hazi06 drop_columns_and_nan working

--- HAZI/HAZI06/HAZI06.py
+++ HAZI/HAZI06/HAZI06.py
@@ -51,3 +51,19 @@ HAZI-
 ##                                                              ##
 ##################################################################
 """
+
+import pandas as pd
+
+
+class NJCleaner:
+    def __init__(self, csv_path: str) -> None:
+        self.data = pd.read_csv(csv_path)
+
+    def order_by_scheduled_time(self):
+        order = self.data.sort_values(by=["scheduled_time"])
+        return order
+
+    def drop_columns_and_nan(self) -> pd.DataFrame:
+        newdata = self.data.drop(["from", "to"], axis=1)
+        newdata.dropna(inplace=True)
+        return newdata
